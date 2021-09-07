226da76c060933c98f6c2208d90bb4b06dd26029
Faith Ikhidero <ikhidero.faithy@gmail.com>
2021-09-07 18:41:24 +0000

    errors fixed

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -5,5 +5,5 @@ if number > 0:
     print("{:d} is positive".format(number))
 elif number == 0:
     print("{:d} is zero".format(number))
-else number < 0:
+else:
     print("{:d} is negative".format(number))
