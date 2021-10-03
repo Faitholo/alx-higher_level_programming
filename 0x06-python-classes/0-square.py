8b902e34286d7b1124c97e40405b2101e1102689
Faitholo <ikhidero.faithy@gmail.com>
2021-10-03 12:00:12 +0100

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+'print(__import__("my_module").my_function.__doc__)'
+python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)')
+
 class Square():
     '''
         An empty square class
