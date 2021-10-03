b303e13c889b37cfa31e30cd265dd6df66153221
Faitholo <ikhidero.faithy@gmail.com>
2021-10-03 12:06:41 +0100

    Delete 0-square.py

--- 0x06-python-classes/0-square.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-'print(__import__("my_module").my_function.__doc__)'
-python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)')
-
-class Square():
-    '''
-        An empty square class
-    '''
-    pass
