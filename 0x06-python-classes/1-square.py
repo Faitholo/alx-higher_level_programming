dce941507f1d9704bed733021d139f296d84ccae
Faitholo <ikhidero.faithy@gmail.com>
2021-10-03 12:07:10 +0100

    Delete 1-square.py

--- 0x06-python-classes/1-square.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-class Square():
-    def __init__(self, size):
-        '''Initialization of instance attributes
-            Args:
-            size (int): The size of the square
-        '''
-        self.__size = size
