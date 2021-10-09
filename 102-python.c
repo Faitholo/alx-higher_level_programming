e72733b68ce059f54e61f1c75a1b3e9646411977
Faitholo <omokhudufaithy@outlook.com>
2021-10-09 06:29:41 +0100

    0x07-python-test_driven_development

---
+++ 102-python.c
@@ -0,0 +1,12 @@
+#include <Python.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_python_string - Prints info about python str
+ * @p: pointer to PyObject
+ * Return: Nothing
+ **/
+void print_python_string(PyObject *p)
+{
+}
