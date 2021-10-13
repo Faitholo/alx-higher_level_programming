83c7a77aa2dd0d9c06415236bf0d1e008640d295
Faitholo <ikhidero.faithy@gmail.com>
2021-10-13 21:19:34 +0100

    Update 6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+'''
+    Implementing a Geometry class
+'''
 
 
 class BaseGeometry:
-
     def area(self):
+        '''
+            Calculating the area
+        '''
         raise Exception("area() is not implemented")
