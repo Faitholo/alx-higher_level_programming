724dbdad0f6690ad6820ff0b58effcf599643c2d
Faitholo <ikhidero.faithy@gmail.com>
2021-12-24 18:17:37 +0100

    making updates

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 import MySQLdb
 
+if __name__ == '__main__':
 
-def get_states(username, password, db_name):
-    '''
-         lists all states with a name starting with N from the database
-    '''
-    db = MySQLdb.connect(host="localhost",
-                         user=username,
-                         passwd=password,
-                         db=db_name,
-                         port=3306)
+    db_user = argv[1]
+    db_passwd = argv[2]
+    db_name = argv[3]
 
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states`\
-                   WHERE `name` REGEXP '^N' ORDER BY `id` ASC")
-    rows = cursor.fetchall()
-    for row in rows:
-        if ("N" in row[1]):
-            print(row)
-    cursor.close()
-    db.close()
+    database = MySQLdb.connect(host='localhost',
+                               port=3306,
+                               user=db_user,
+                               passwd=db_passwd,
+                               db=db_name)
 
-if __name__ == "__main__":
-    credentials = sys.argv
-    username = sys.argv[1]
-    passwd = sys.argv[2]
-    db_name = sys.argv[3]
-    get_states(username, passwd, db_name)
+    cursor = database.cursor()
+
+    cursor.execute('SELECT id, name FROM states ORDER BY states.id ASC')
+
+    for row in cursor.fetchall():
+        print(row)
