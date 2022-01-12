7eb97a42455f6b2e385fb11ccf44d1107c12d60f
Faitholo <ikhidero.faithy@gmail.com>
2022-01-12 17:05:05 +0100

    made updates

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-# Python script that fetches https://intranet.hbtn.io/status
-
+"""A script that
+fetches https://intranet.hbtn.io/status.
+"""
 from urllib.request import Request, urlopen
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    req = Request(url)
-    with urlopen(req) as response:
-        page = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(page)))
-    print("\t- content: {}".format(page))
-    print("\t- utf8 content: {}".format(page.decode("utf-8")))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+        content = resp.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
