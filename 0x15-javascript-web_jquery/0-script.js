35afc78ce8f4cfbf67ebaac52aae8fcd4bafafde
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-02-09 19:09:57 +0100

    JQuery

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,4 @@
+// Javascript script that updates the text color of the <header> element to red (#FF0000)
+
+const colorH = document.querySelector('header');
+colorH.style.color = '#FF0000';
---
+++ 0x15-javascript-web_jquery/README.md
@@ -0,0 +1,1 @@
+Project JavaScript Web JQuery
